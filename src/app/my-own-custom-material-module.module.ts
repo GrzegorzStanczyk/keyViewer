e59d8659ea6e516962612b6cda4dbca7a2461d9f
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-07-26 22:36:00 +0200

    Tested material burrons ftw

--- src/app/my-own-custom-material-module.module.ts
+++ src/app/my-own-custom-material-module.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { 
+  MdButtonModule, 
+  MdCheckboxModule, 
+  MdToolbarModule, 
+  MdIconModule, 
+  MdButtonToggleModule, 
+  MdSidenavModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MdButtonModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonToggleModule, 
+    MdSidenavModule
   ],
-  exports: [MdButtonModule, MdCheckboxModule],
+  exports: [
+    MdButtonModule, 
+    MdCheckboxModule, 
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonToggleModule, 
+    MdSidenavModule
+  ],
   declarations: []
 })
 export class MyOwnCustomMaterialModuleModule { }
