8098d2d91e5f9999ffe923ce876f6847b8984996
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-04 19:08:19 +0200

    Bad DRY components styling, to solve

--- src/app/key-settings/key-settings.component.ts
+++ src/app/key-settings/key-settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-key-settings',
   templateUrl: './key-settings.component.html',
-  styleUrls: ['./key-settings.component.css']
+  styleUrls: ['./key-settings.component.scss']
 })
 export class KeySettingsComponent implements OnInit {
 
