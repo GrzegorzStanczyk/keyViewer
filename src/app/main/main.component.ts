a813b25b487443a08f90c21d96b27ba0f859e39b
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-04 19:05:16 +0200

    Added map and key component to the main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+
+    var mapProp = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    }
 
 }
