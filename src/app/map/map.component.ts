3da269496d9d7dad66693d284eeb49f31e79007b
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-08 21:08:08 +0200

    Added marker and centering map when search place

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -6,21 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @ViewChild('googleMap') el:ElementRef;
   constructor() { }
 
-  getPlaceOnChange(e) {
-    console.log(e)
+  getPlaceOnChange(place) {
+    console.log(place)
+
+    const map = new google.maps.Map(this.el.nativeElement)
+    var marker = new google.maps.Marker();
+    marker.set('map', map);
+    marker.set('anchorPoint', new google.maps.Point(0, -29));
+
+    if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+      if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+      }
+
+      marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
   }
 
   loadMap() {
-      var mapProp = {
+      const mapProp = {
             center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 5,
+            zoom: 17,
             streetViewControl: false,
             mapTypeControl: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        const map = new google.maps.Map(this.el.nativeElement, mapProp);
   }
 
   ngOnInit() {
