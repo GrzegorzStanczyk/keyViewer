b807c37b9e176d8195271212cc90068461000b6b
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-10 13:47:41 +0200

    Provide coords to key.c, and listen changes by ngZone to update view

--- src/app/key/key.component.ts
+++ src/app/key/key.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { KeyService } from './key.service';
+import { MapLoaderService } from '../map/map-loader.service';
 import { Key } from './key.model';
 
 
@@ -10,29 +12,42 @@ import { Key } from './key.model';
   styleUrls: ['./key.component.scss'],
   providers: [KeyService]
 })
-export class KeyComponent implements OnInit {
-  
-  markerFiltered: Promise<Key> | null = null;  
-  // key: Promise<string>;  
+export class KeyComponent implements OnInit, OnDestroy {  
+  markerFiltered: Promise<{}> | null = null; 
+   
+  subscriptionToGetCoords: Subscription;
 
-  constructor(private keyService: KeyService) { }
+  constructor(
+    private keyService: KeyService, 
+    private mapLoaderService: MapLoaderService, 
+    private zone:NgZone) { 
 
-  public findNearest(cords) {
-    const userLatLng = new google.maps.LatLng(cords.lat, cords.lng);
-    this.markerFiltered = this.keyService.getKeys()
+    this.subscriptionToGetCoords = this.mapLoaderService.getCoords()
+      .subscribe(coords => { 
+        this.findNearest(coords)
+        this.zone.run(() => this.findNearest(coords));
+      })
+  }
+
+  public findNearest(coords): Promise<{}> {
+    const userLatLng = new google.maps.LatLng(coords.lat, coords.lng);
+    return this.markerFiltered = this.keyService.getKeys()    
       .then(keys => {
-         return keys.reduce(function (prev, curr) {          
+         return keys.reduce(function (prev, curr) {
           let location1 = new google.maps.LatLng(prev.lat, prev.lng)
           let location2 = new google.maps.LatLng(curr.lat, curr.lng)
-          var ppos = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, location1);
-          var cpos = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, location2);
+          let ppos = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, location1);
+          let cpos = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, location2);
           return cpos < ppos ? curr : prev;
         })
       })
   }
 
   ngOnInit() { 
-    this.findNearest({lat: 52.235981, lng: 20.902539})
+  }
+
+  ngOnDestroy() {
+    this.subscriptionToGetCoords.unsubscribe();
   }
 
 }
