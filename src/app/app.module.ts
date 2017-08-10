961b3c5ea3c2efb23cf74d441bcda4148635e9f9
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-10 21:41:08 +0200

    Added ngx-translate languages switcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 import {} from '@types/googlemaps';
+// import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Http, HttpModule }       from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -15,12 +17,19 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ComponentViewerComponent } from './component-viewer/component-viewer.component';
 import { KeyComponent } from './key/key.component';
+import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
 
 import { MyOwnCustomMaterialModuleModule } from './material-module/my-own-custom-material-module.module';
 import { AppRoutingModule } from './app-routing.module';
 import { MapComponent } from './map/map.component';
 import { AutocompleteDirective } from './map/autocomplete.directive';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+	import { ConnectionBackend } from '@angular/http';
 
+export function createTranslateLoader(http: Http) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -36,14 +45,24 @@ import { AutocompleteDirective } from './map/autocomplete.directive';
     ComponentViewerComponent,
     KeyComponent,
     MapComponent,
-    AutocompleteDirective
+    AutocompleteDirective,
+    LanguageSwitcherComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MyOwnCustomMaterialModuleModule
+    MyOwnCustomMaterialModuleModule,
+    HttpModule, 
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [Http]
+      }
+    })
   ],
+  schemas: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/language-switcher/language-switcher.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.scss']
+})
+export class LanguageSwitcherComponent implements OnInit {
+
+  constructor(private translate: TranslateService) {
+        this.translate.addLangs(["en", "pl"]);
+        // this.translate.setDefaultLang('en');
+
+        let browserLang = translate.getBrowserLang();
+        this.translate.use(browserLang.match(/en|pl/) ? browserLang : 'en');
+    }
+
+  ngOnInit() {}
+
+}
