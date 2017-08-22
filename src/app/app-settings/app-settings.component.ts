ba99d73595612f8a70dfe10b497d681f1bbb132b
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-22 10:29:52 +0200

    Added support for the sekected theme

--- src/app/app-settings/app-settings.component.ts
+++ src/app/app-settings/app-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ThemePickerService } from './theme-picker.service';
+
 @Component({
   selector: 'app-settings',
   templateUrl: './app-settings.component.html',
@@ -9,14 +11,19 @@ export class AppSettingsComponent implements OnInit {
   isDarkTheme: boolean = false;
   isOpen: boolean = false;
 
-  constructor() { }
+  constructor(private themePickerService: ThemePickerService) { }
 
   toggle() {
     this.isOpen = !this.isOpen;
   }
 
+  setTheme(theme: string): void {
+    this.themePickerService.sendPickedTheme(theme);
+  }
+
   ngOnInit() {
   }
+  
   changeTheme(): void {
     if (this.isDarkTheme) {
        this.isDarkTheme = false;
