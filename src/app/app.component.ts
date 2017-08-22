f5202aec9dd27253f1c592a620266b5240b2662f
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-08-22 10:31:15 +0200

    Added subscription to picked theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppContent } from './content';
 import { OverlayContainer } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
 
-import { SidenavOpenService } from './sidenav/sidenav-open.service'
+import { SidenavOpenService } from './sidenav/sidenav-open.service';
+import { ThemePickerService } from './app-settings/theme-picker.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [SidenavOpenService]
+  providers: [SidenavOpenService, ThemePickerService]
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   content: Object = AppContent;
   isDarkTheme: boolean = false;
   themeClass: string;
   newThemeClass: string = "unicorn-dark-theme";
-  
-  constructor(private router: Router, private overlayContainer: OverlayContainer) {}
+  subscription: Subscription;
 
-  changeTheme(): void {
-    if (this.isDarkTheme) {
-       this.isDarkTheme = false;
-    } else {
-       this.isDarkTheme = true;
-    }
- }
+  constructor(
+    private router: Router,
+    private overlayContainer: OverlayContainer,
+    private themePickerService: ThemePickerService) {
+  }
 
- ngOnInit():void {
-  this.themeClass = "pink-bluegrey";
-  this.overlayContainer.themeClass = "candy-theme";
- }
+  ngOnInit(): void {
+    this.subscription = this.themePickerService.getPickedtheme()
+      .subscribe(theme => {
+        this.newThemeClass = theme;
+        console.log("newThemeClass", this.newThemeClass)
+        this.themeClass = this.newThemeClass;
+        this.overlayContainer.themeClass = this.newThemeClass;
+      });
+    // this.themeClass = "pink-bluegrey";
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
