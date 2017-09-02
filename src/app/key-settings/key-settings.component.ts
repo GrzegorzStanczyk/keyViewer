959652a3f6d5c69cc6044378b395648a179815dd
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-09-02 15:53:36 +0200

    Provided valid key to display in form settings

--- src/app/key-settings/key-settings.component.ts
+++ src/app/key-settings/key-settings.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { slideInDownAnimation, slideInOutAnimation, slideToTop } from '../router.animations';
 
+import { Key } from '../key/key.model';
+import { KEYS } from '../key/key-mock';
+
 import { SidenavOpenService } from '../sidenav/sidenav-open.service';
+import { DataStorageService } from './data-storage.service';
+import { KeyService } from '../key/key.service';
+import { MapLoaderService } from '../map/map-loader.service';
+
 
 @Component({
   selector: 'app-key-settings',
@@ -12,14 +18,32 @@ import { SidenavOpenService } from '../sidenav/sidenav-open.service';
   host: { '[@routerTransition]': '' }
 })
 export class KeySettingsComponent implements OnInit {
+  key: Key;
 
-  constructor(private sidenavOpenService: SidenavOpenService) { }
-
+  constructor(
+    private sidenavOpenService: SidenavOpenService,
+    private dataStorageService: DataStorageService,
+    private keyService: KeyService) { 
+      if(this.keyService.keyToEdit !== null) {   
+        this.keyService.keyToEdit
+          .then((data)=>{
+            this.key = data;
+          })        
+      } else {
+        this.key = new Key('', null, null, null, '');
+      }
+    }
+  
   closeAndGoToMain() {
-    this.sidenavOpenService.sendKeySettings();
+    // this.sidenavOpenService.sendKeySettings();
+    this.dataStorageService.storeKey()
+      .subscribe(data => console.log(data));
   }
 
-  ngOnInit() {
+  getKey() {
+    this.dataStorageService.getKey();
   }
+
+  ngOnInit() {}
 
 }
