4d5b6b835b167010d4a645fb42f28e18a9fdf012
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-09-02 15:27:30 +0200

    Added key-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { KeySettingsComponent } from './key-settings/key-settings.component';
 import { AppSettingsComponent } from './app-settings/app-settings.component';
 import { AboutComponent } from './about/about.component';
+import { KeysListComponent } from './keys-list/keys-list.component';
 
 const routes: Routes = [
   { path: 'main',  component: MainComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'keys-list', component: KeysListComponent }, 
   { path: 'key-settings', component: KeySettingsComponent },
-  { path: 'app-settings', component: AppSettingsComponent }, 
+  { path: 'app-settings', component: AppSettingsComponent },
   { path: 'about',  component: AboutComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: '**', redirectTo: '/main', pathMatch: 'full' }
---
+++ src/app/keys-list/keys-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+
+
+@Component({
+  selector: 'app-keys-list',
+  templateUrl: './keys-list.component.html',
+  styleUrls: ['./keys-list.component.scss']
+})
+
+export class KeysListComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  item: FirebaseObjectObservable<any>;
+  i = 0;
+
+  constructor(private db: AngularFireDatabase) {
+    this.items = db.list('/');
+    this.findAllLessons();
+  }
+
+  findAllLessons() {
+    return this.db.list('/keys')
+        .subscribe(v => console.log("v", v));
+  }
+
+  addItem() {
+    this.i++;
+    const key = this.db.object(`/${this.i}`);
+    key.set({ "streetName": "Lazurowa 40, 01-315 Warszawa", "lat": 52.238957, "lng": 20.89739, "radius": 10, "key": "Orlen" });
+    // this.items.push({"streetName":"Lazurowa 40, 01-315 Warszawa","lat":52.238957,"lng":20.89739,"radius":10,"key":"Orlen"});
+  }
+  updateItem() {
+    this.i++;
+    this.items.update(this.i.toString(), { "streetName": "Lazurowa 40, 01-315 Warszawa", "lat": 52.238957, "lng": 20.89739, "radius": 10, "key": "Orlen" });
+  }
+  deleteItem() {
+    console.log(this.i)
+    const key = this.db.object(`/keys/${this.i}`);
+    key.remove();
+    this.i--;
+
+    // this.items.remove(key); 
+  }
+  deleteEverything() {
+    // this.items.remove();
+  }
+
+  ngOnInit() {
+  }
+
+}
