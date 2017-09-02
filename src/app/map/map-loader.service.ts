3631e6614c2b8dea67e8e2d07e8e5bd90673d02c
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-09-02 21:44:12 +0200

    Separated logic feom map.component in to service

--- src/app/map/map-loader.service.ts
+++ src/app/map/map-loader.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Subject }    from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
 
+import { Key } from '../key/key.model';
 
 const url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAYHIBJW0pmB5AYDPWsugpqzMN2Ugg_yqU&libraries=places,geometry&callback=__onGoogleLoaded';
 
@@ -9,7 +10,7 @@ const url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAYHIBJW0pmB5AYDPW
 export class MapLoaderService {
   public static promise: Promise<any>;
   public mapSubjectSource = new Subject<any>();
-  
+
   public sendCoords(obj): void {
     this.mapSubjectSource.next(obj);
   }
@@ -20,26 +21,75 @@ export class MapLoaderService {
 
   public load(): Promise<any> {
     // First time 'load' is called?
-    if(!MapLoaderService.promise) {
+    if (!MapLoaderService.promise) {
 
       // Make promise to load.
       MapLoaderService.promise = new Promise((resolve) => {
-        
+
         // Set callback for when google maps is loaded.
         window['__onGoogleLoaded'] = () => {
           resolve('google maps api loaded');
         };
-        
+
         let node = document.createElement('script');
         node.src = url;
         node.type = 'text/javascript';
         node.setAttribute('async', '');
-        node.setAttribute('defer', '');        
+        node.setAttribute('defer', '');
         document.getElementsByTagName('head')[0].appendChild(node);
-      });      
+      });
     }
 
     // Always return promise. When 'load' is called many times, the promise is already resolved.
     return MapLoaderService.promise
+  }
+
+  public getCurrentPosition(): Promise<any> {
+    let lat: number;
+    let lng: number;
+    return new Promise(resolve => {
+      if ("geolocation" in navigator) {
+        return navigator.geolocation.getCurrentPosition((position) => {
+          lat = position.coords.latitude;
+          lng = position.coords.longitude;
+          resolve({ lat: lat, lng: lng })
+        }, this.showError, { enableHighAccuracy: true });
+      }
+    })
+  }
+
+  public showError(error): void {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        console.log("An unknown error occurred.")
+        break;
+    }
+  }  
+
+  public getStreetName(cords): Promise<Key> {
+    const geocoder = new google.maps.Geocoder;
+
+    return new Promise(resolve => {
+      geocoder.geocode({ 'location': { lat: cords.lat, lng: cords.lng } }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            resolve(new Key(results[0].formatted_address, cords.lat, cords.lng, null, null));
+          } else {
+            window.alert('No results found');
+          }
+        } else {
+          window.alert('Geocoder failed due to: ' + status);
+        }
+      });
+    })
   }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 import { MapLoaderService } from './map-loader.service';
+import { KeyService } from '../key/key.service';
 
 @Component({
   selector: 'app-map',
@@ -10,11 +11,11 @@ import { MapLoaderService } from './map-loader.service';
 export class MapComponent implements OnInit {
   @ViewChild('googleMap') el: ElementRef;
 
-  lat: number;
-  lng: number;
   mapReady: boolean = null;
 
-  constructor(private mapLoaderService: MapLoaderService) { }
+  constructor(
+    private mapLoaderService: MapLoaderService,
+    private keyService: KeyService) { }
 
   setMarker(map) {
     const marker = new google.maps.Marker();
@@ -44,8 +45,14 @@ export class MapComponent implements OnInit {
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
 
-    //Send coordinates to the key.component    
-    this.mapLoaderService.sendCoords({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() });
+    const coords = {
+      lat: place.geometry.location.lat(), 
+      lng: place.geometry.location.lng()
+    }
+    // Send coordinates to the key.component    
+    this.mapLoaderService.sendCoords(coords);
+    // Provide street name and coords to global variable keyToEdit
+    this.keyService.keyToEdit = this.mapLoaderService.getStreetName(coords);
   }
 
   loadMap(coords): void {
@@ -65,47 +72,30 @@ export class MapComponent implements OnInit {
     marker.setVisible(true);
   }
 
-  getCurrentPosition(): Promise<any> {
-    return new Promise(resolve => {
-      if ("geolocation" in navigator) {
-        return navigator.geolocation.getCurrentPosition((position) => {
-          this.lat = position.coords.latitude;
-          this.lng = position.coords.longitude;
-          resolve({ lat: this.lat, lng: this.lng })
-        }, this.showError, { enableHighAccuracy: true });
-      }
-    })
-  }
-
-  showError(error): void {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        console.log("User denied the request for Geolocation.")
-        break;
-      case error.POSITION_UNAVAILABLE:
-        console.log("Location information is unavailable.")
-        break;
-      case error.TIMEOUT:
-        console.log("The request to get user location timed out.")
-        break;
-      case error.UNKNOWN_ERROR:
-        console.log("An unknown error occurred.")
-        break;
-    }
-  }
-
-  localizateMe() {
-    this.getCurrentPosition()
+  localizateMe(): void {
+    this.mapLoaderService.getCurrentPosition()
     .then(coords => {
       //Send coordinates to the key.component
       this.mapLoaderService.sendCoords({ lat: coords.lat, lng: coords.lng });
+      
+      this.keyService.keyToEdit = this.mapLoaderService.getStreetName(coords);
       this.loadMap(coords);
     });
   }
-
+  
+  // init(): void {
+  //   this.mapLoaderService.getCurrentPosition()
+  //   .then(coords => {
+  //     //Send coordinates to the key.component
+  //     this.mapLoaderService.sendCoords({ lat: coords.lat, lng: coords.lng });
+      
+  //     this.keyService.keyToEdit = this.mapLoaderService.getStreetName(coords);
+  //     this.loadMap(coords);
+  //   });
+  // }
+  
   ngOnInit() {
     if (typeof google !== 'undefined') {
-      console.log('MapComponent.ngOnInit');
       this.localizateMe()
     }
   }
