956a2e17dbc016b868ffe5b686c9a816cceb5793
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-09-05 22:38:32 +0200

    Work on db

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -25,15 +25,36 @@ export class DataStorageService {
   }
   
 
-  storeKey() {
-    // this.items.remove();
-    // KEYS.forEach(element => {
-    //   this.items.push(element)
-    // });
+  storeKey(key: Key) {
+    const newKey = new Key(key.streetName, key.lat, key.lng, 10, key.key, key.note);
+    
+    this.items.remove();
+    KEYS.forEach(element => {
+      this.items.push(element)
+    });
 
     // return this.items.update("2", { "streetName": "Lazurowa 40, 01-315 Warszawa", "lat": 52.238957, "lng": 20.89739, "radius": 10, "key": "Orlen", note: "notatka"});
-    // return this.items.push( { "streetName": "Lazurowa 40, 01-315 Warszawa", "lat": 52.238957, "lng": 20.89739, "radius": 10, "key": "Orlen", note: "notatka"});
+    // this.items.push( { "streetName": "Lazurowa 40, 01-315 Warszawa", "lat": 52.238957, "lng": 20.89739, "radius": 10, "key": "Orlen", note: "notatka"});
+    // this.items.update(key.streetName, newKey);
+  }
+
+  editKey() {
+    const newKey =new Key('Amfiteatr im. Michaela Jacksona', 52.233631, 20.906177, 10, '100#2580', null);
     
+    // this.items.remove();
+    // KEYS.forEach(element => {
+    //   let toSend = this.db.object(`/${element.streetName}`);
+    //   toSend.set(element);
+    // });
+    // ".", "#", "$", "[", or "]"
+    this.items.remove();
+    KEYS.forEach(element => {
+      // const properStreetName = element.streetName.replace(/\.|#|\$|\[|]/gm,',');
+      const properStreetName = element.streetName.split(/\.|#|\$|\[|]/gm).join(",");
+      
+      const toSend = this.db.object(`/${properStreetName}`);
+      toSend.set(element);
+    });
   }
 
   getKeys(): Promise<Key[]> {
